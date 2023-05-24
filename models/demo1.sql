df2241d3e454677e4a51615664ebcd15807c0f0f
Ramakantdbt <131665164+Ramakantdbt@users.noreply.github.com>
2023-05-24 15:00:41 +0530

    Update demo1.sql

--- models/demo1.sql
+++ models/demo1.sql
@@ -1,5 +1,6 @@
 /*branch development_1*/
 /*test commented added on 24 May*/
+/*conflict comment added by another person*/
 Select * from SNOWFLAKE.ACCOUNT_USAGE.ACCESS_HISTORY
 union all
 Select * from SNOWFLAKE.ACCOUNT_USAGE.ACCESS_HISTORY
