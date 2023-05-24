4630e76e896a7b7e2c32aacbf0d2fa17bb00adfe
ramakant.divate@fosfor.com <ramakant.divate@fosfor.com>
2023-05-24 09:31:07 +0000

    observer conflict

--- models/demo1.sql
+++ models/demo1.sql
@@ -1,5 +1,6 @@
 /*branch development_1*/
 /*test commented added on 24 May*/
+/*second test comment added on 24 May*/
 Select * from SNOWFLAKE.ACCOUNT_USAGE.ACCESS_HISTORY
 union all
 Select * from SNOWFLAKE.ACCOUNT_USAGE.ACCESS_HISTORY
