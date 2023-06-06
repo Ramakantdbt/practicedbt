3823b3955681a0355f088d97f70352cfd3ecc7e8
ramakant.divate@fosfor.com <ramakant.divate@fosfor.com>
2023-06-06 06:30:15 +0000

    ignored main pull

--- models/demo1.sql
+++ models/demo1.sql
@@ -1,6 +1,7 @@
 /*branch development_1*/
 /*rebase check*/
 /*sync_check.sql*/
+/*ignore main pull*/
 Select * from SNOWFLAKE.ACCOUNT_USAGE.ACCESS_HISTORY
 union all
 Select * from SNOWFLAKE.ACCOUNT_USAGE.ACCESS_HISTORY
